916de90b187bded9ebd46682101c487f33d00735
Anemieke <69979253+anemiekeZA@users.noreply.github.com>
2023-05-31 22:48:46 +0200

    Update pigGame.js

--- pigGame/pigGame.js
+++ pigGame/pigGame.js
@@ -1,17 +1,17 @@
 'use strict';
 
 // selecting Elements
-const player0El = documemt.querySelector('.player--0');
-const player0El = documemt.querySelector('.player--1');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById ('score--1');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
 
 const diceEl = document.querySelector('.dice');
-const btnNew = document.querySelector('.btn btn--new');
-const btnRoll = document.querySelector('.btn btn--roll');
-const btnHold = document.querySelector('.btn btn--hold');
+const btnNew = document.querySelector('.btn.btn--new');
+const btnRoll = document.querySelector('.btn.btn--roll');
+const btnHold = document.querySelector('.btn.btn--hold');
 
 // Starting conditions
 score0El.textContent = 0;
